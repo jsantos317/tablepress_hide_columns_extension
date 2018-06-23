e26b6ee8a092f5d5ce607bcf61563a24f767cbbc
Joseph Santos <jsantos317@gmail.com>
2018-06-23 10:55:41 -0500

    Add shortcode for each role to hide column/row

--- tablepress-hide-columns-rows-logged-out-users.php
+++ tablepress-hide-columns-rows-logged-out-users.php
@@ -1,14 +1,26 @@
 <?php
 /*
-Plugin Name: TablePress Extension: Hide Columns/Rows from logged-out users
-Plugin URI: https://tablepress.org/extensions/hide-data-logged-out-users/
-Description: Custom Extension for TablePress that allows to hide certain columns/rows from logged-out users
+Plugin Name: TablePress Extension: Hide Columns/Rows from user roles
+Plugin URI:
+Description: Custom Extension for TablePress that allows to hide certain columns/rows from users with certain roles
 Version: 1.0
-Author: Tobias Bäthge
-Author URI: https://tobias.baethge.com/
+Author: Joseph Santos
+Author URI: http://codeunicorn.com/
 */
-add_filter( 'tablepress_shortcode_table_default_shortcode_atts', 'tablepress_add_shortcode_parameter_hide_logged_out' );
-add_filter( 'tablepress_table_render_options', 'tablepress_hide_columns_from_logged_out_users', 10, 2 );
+add_filter( 'tablepress_shortcode_table_default_shortcode_atts', 'tablepress_add_shortcode_parameter_hide_role' );
+add_filter( 'tablepress_table_render_options', 'tablepress_render_columns_from_user_role', 10, 2 );
+
+/**
+ * @return array Set of editable roles
+ */
+function tablepress_get_editable_roles() {
+    global $wp_roles;
+
+    $all_roles = $wp_roles->roles;
+    $editable_roles = apply_filters('editable_roles', $all_roles);
+
+    return $editable_roles;
+}
 
 /**
  * Add `hide_columns_logged_out` and `hide_rows_logged_out` as valid Shortcode parameters.
@@ -18,10 +30,20 @@ add_filter( 'tablepress_table_render_options', 'tablepress_hide_columns_from_log
  * @param array $default_atts Current set of Shortcode parameters.
  * @return array Extended set of Shortcode parameters.
  */
-function tablepress_add_shortcode_parameter_hide_logged_out( $default_atts ) {
-	$default_atts['hide_columns_logged_out'] = '';
-	$default_atts['hide_rows_logged_out'] = '';
-	return $default_atts;
+function tablepress_add_shortcode_parameter_hide_role( $default_atts ) {
+
+    $editable_roles = tablepress_get_editable_roles();
+
+    foreach( $editable_roles as $role => $roleArray ) {
+
+        $attrCol = 'hide_columns_'. $role;
+        $attrRow = 'hide_rows_'. $role;
+
+        $default_atts[$attrCol] = '';
+        $default_atts[$attrRow] = '';
+    }
+
+    return $default_atts;
 }
 
 /**
@@ -33,27 +55,31 @@ function tablepress_add_shortcode_parameter_hide_logged_out( $default_atts ) {
  * @param array $table          The table.
  * @return array The modified render options.
  */
-function tablepress_hide_columns_from_logged_out_users( $render_options, $table ) {
-	if ( is_user_logged_in() ) {
-		return $render_options;
-	}
+function tablepress_render_columns_from_user_role( $render_options, $table ) {
 
-	if ( ! empty( $render_options['hide_columns_logged_out'] ) ) {
-		if ( empty( $render_options['hide_columns'] ) ) {
-			$render_options['hide_columns'] = $render_options['hide_columns_logged_out'];
-		} else {
-			$render_options['hide_columns'] .= ',' . $render_options['hide_columns_logged_out'];
-		}
-	}
+    // var_dump($render_options);
 
-	if ( ! empty( $render_options['hide_rows_logged_out'] ) ) {
-		if ( empty( $render_options['hide_rows'] ) ) {
-			$render_options['hide_rows'] = $render_options['hide_rows_logged_out'];
-		} else {
-			$render_options['hide_rows'] .= ',' . $render_options['hide_rows_logged_out'];
-		}
-	}
+    $editable_roles = tablepress_get_editable_roles();
+    foreach( $editable_roles as $role => $roleArray ) {
+
+        $render_options = render_helper($render_options, 'hide', 'columns', $role);
+        $render_options = render_helper($render_options, 'hide', 'rows', $role);
+        $render_options = render_helper($render_options, 'show', 'columns', $role);
+        $render_options = render_helper($render_options, 'show', 'rows', $role);
+
+    }
 
 	return $render_options;
 }
 
+function render_helper($render_options, $hide_show, $row_col, $role) {
+    if ( ! empty( $render_options[$hide_show .'_'. $row_col .'_'. $role] ) ) {
+        if ( empty( $render_options[$hide_show .'_'. $row_col] ) ) {
+            $render_options[$hide_show .'_'. $row_col] = $render_options[$hide_show .'_'. $row_col .'_'. $role];
+        } else {
+            $render_options[$hide_show .'_'. $row_col] .= ',' . $render_options[$hide_show .'_'. $row_col .'_'. $role];
+        }
+    }
+
+    return $render_options;
+}
